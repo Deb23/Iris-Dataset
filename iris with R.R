31fb0152b76ce64203412441adc73e94d5aff224
Deb23 <debasmita.23b@gmail.com>
2016-12-28 11:18:23 -0500

    Add files via upload

---
+++ iris with R.R
@@ -0,0 +1,82 @@
+library(caret)
+
+data("iris")
+dataset <- iris
+
+
+validation_index <- createDataPartition(dataset$Species, p=0.80, list=FALSE)
+validation <- dataset[-validation_index,]
+dataset <- dataset[validation_index,]
+
+# list types for each attribute
+sapply(dataset, class)
+
+
+# summarize the class distribution
+percentage <- prop.table(table(dataset$Species)) * 100
+cbind(freq=table(dataset$Species), percentage=percentage)
+
+# summarize attribute distributions
+summary(dataset)
+
+# Visualization
+# split input and output
+x <- dataset[,1:4]
+y <- dataset[,5]
+
+# Univariate plots to study the distribution of each attribute
+# boxplot for each attribute on one image
+par(mfrow=c(1,4))
+for(i in 1:4) {
+  boxplot(x[,i], main=names(iris)[i])
+}
+
+# barplot for class breakdown
+plot(y)
+
+# Multivariate plots to study the interaction between variables
+# scatterplot matrix
+featurePlot(x=x, y=y, plot="ellipse")
+
+plot(y~x)
+plot(x)
+
+# density plots for each attribute by class value
+scales <- list(x=list(relation="free"), y=list(relation="free"))
+featurePlot(x=x, y=y, plot="density", scales=scales)
+
+# Run algorithms using 10-fold cross validation
+control <- trainControl(method="cv", number=10)
+metric <- "Accuracy"
+
+# a) linear algorithms
+set.seed(7)
+fit.lda <- train(Species~., data=dataset, method="lda", metric=metric, trControl=control)
+# b) nonlinear algorithms
+# CART
+set.seed(7)
+fit.cart <- train(Species~., data=dataset, method="rpart", metric=metric, trControl=control)
+# kNN
+set.seed(7)
+fit.knn <- train(Species~., data=dataset, method="knn", metric=metric, trControl=control)
+# c) advanced algorithms
+# SVM
+set.seed(7)
+fit.svm <- train(Species~., data=dataset, method="svmRadial", metric=metric, trControl=control)
+# Random Forest
+set.seed(7)
+fit.rf <- train(Species~., data=dataset, method="rf", metric=metric, trControl=control)
+
+# summarize accuracy of models
+results <- resamples(list(lda=fit.lda, cart=fit.cart, knn=fit.knn, svm=fit.svm, rf=fit.rf))
+summary(results)
+
+# compare accuracy of models
+dotplot(results)
+
+# summarize Best Model
+print(fit.lda)
+
+# estimate skill of LDA on the validation dataset
+predictions <- predict(fit.lda, validation)
+confusionMatrix(predictions, validation$Species)
